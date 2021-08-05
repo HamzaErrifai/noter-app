c374b1773a61444e88e154066a50b71640c7c2e2
Hamza Hamza <hamzaerrifai@hotmail.com>
2021-08-05 13:32:15 +0100

    added nav to first page

---
+++ resources/views/layouts/menu.blade.php
@@ -0,0 +1,7 @@
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <div class="d-flex justify-content-center" style="width: 100%">
+      <a class="navbar-brand text-center" href="/">
+          <h2 style="color: tomato; font-family:'Nunito';">NoterApp</h2>
+      </a>
+  </div>
+</nav>
